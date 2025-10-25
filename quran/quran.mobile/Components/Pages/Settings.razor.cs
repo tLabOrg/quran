563565a3bf89795ada6c3797792d6ae0b46a39db
يس سليمان <yassin_soliman@outlook.com>
2025-10-26 02:17:17 +0300

    Added rate app action

--- quran/quran.mobile/Components/Pages/Settings.razor.cs
+++ quran/quran.mobile/Components/Pages/Settings.razor.cs
@@ -12,9 +12,36 @@ namespace quran.mobile.Components.Pages
             Nav.NavigateTo("/");
         }
 
+        async Task OnRatePressed()
+        {
+            string url = string.Empty;
+#if ANDROID
+            url = "https://play.google.com/store/apps/details?id=org.arbweb.moratal";
+#elif IOS
+            url = "https://apps.apple.com/us/app/moratal/id310633997";
+#elif WINDOWS
+            url = "ms-windows-store://pdp/?productid=9NC0NPGCRXRH";
+#endif
+            await Launcher.OpenAsync(url);
+        }
+
+        async Task OnSupportPressed() 
+        {
+            await Launcher.OpenAsync("https://arbweb.org/support_proj.html");
+        }
+
         async Task OnChatPressed()
         {
             await Launcher.OpenAsync("https://wa.me/201031160910");
+        }
+
+        async Task OnSharePressed()
+        {
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Title = "مشاركة تطبيق المصحف المرتل",
+                Text = "https://play.google.com/store/apps/details?id=org.arbweb.moratal"
+            });
         }
     }
 }
--- quran/quran.mobile/State.cs
+++ quran/quran.mobile/State.cs
@@ -7,5 +7,12 @@ namespace quran.mobile
         public static Data data { get; set; } = new();
         public static string searchText = string.Empty;
         public static Reciter? selectedReciter = null;
+
+        public static void Reset()
+        {
+            data = new Data();
+            searchText = string.Empty;
+            selectedReciter = null;
+        }
     }
 }
